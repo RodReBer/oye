01e7e7377be687b3ef4d3f0b2abbe9905f11d36d
Rodrigo Rey <rodrigorey2005@gmail.com>
2023-11-23 09:19:34 -0300

    ñ

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,103 +1,116 @@
-import { NavBar, Footer, Team } from '../components/index'
+import { NavBar, Footer, Team } from "../components/index";
 
-import {
-    CheckCircleIcon,
-} from '@heroicons/react/20/solid'
+import { CheckCircleIcon } from "@heroicons/react/20/solid";
 
 const stats = [
-    { label: 'Año', value: '2023' },
-    { label: 'Curso', value: 'PyE' },
-    { label: 'Profe', value: 'Freddy' },
-    { label: 'Grupo', value: 'M3A' },
-]
+  { label: "Año", value: "2023" },
+  { label: "Curso", value: "PyE" },
+  { label: "Profesor", value: "Freddy" },
+  { label: "Grupo", value: "M3A" },
+];
 
 const benefits = [
-    'Competitive salaries',
-    'Flexible work hours',
-    '30 days of paid vacation',
-    'Annual team retreats',
-    'Benefits for you and your family',
-    'A great work environment',
-]
+  "Competitive salaries",
+  "Flexible work hours",
+  "30 days of paid vacation",
+  "Annual team retreats",
+  "Benefits for you and your family",
+  "A great work environment",
+];
 
 const Home = () => {
-    return (
-        <div className="bg-gray-900">
-            {/* Header */}
+  return (
+    <div className="bg-gray-900">
+      {/* Header */}
 
-            <NavBar />
+      <NavBar />
 
-            <main className="relative isolate">
-                {/* Background */}
+      <main className="relative isolate">
+        {/* Background */}
+        <div
+          className="absolute inset-x-0 top-4 -z-10 flex transform-gpu justify-center overflow-hidden blur-3xl"
+          aria-hidden="true"
+        >
+          <div
+            className="aspect-[1108/632] w-[69.25rem] flex-none bg-gradient-to-r from-[#80caff] to-[#11f120] opacity-25"
+            style={{
+              clipPath:
+                "polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)",
+            }}
+          />
+        </div>
+
+        {/* Header section */}
+        <div className="px-6 pt-14 lg:px-8">
+          <div className="mx-auto max-w-2xl pt-24 text-center sm:pt-40">
+            <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+              Probabilidad y estad&iacute;stica
+            </h2>
+            <p className="mt-6 text-lg leading-8 text-gray-300">
+              Curso de probabilidad y estadistica de la Universidad Uruguay ORT
+              2023
+            </p>
+          </div>
+        </div>
+
+        {/* Content section */}
+        <div className="mx-auto mt-20 max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
+            <div className="grid max-w-xl grid-cols-1 gap-8 text-base leading-7 text-gray-300 lg:max-w-none lg:grid-cols-2">
+              <div>
+                <p>
+                  Nos complace darle la bienvenida a nuestro proyecto centrado
+                  en las precisiones estadísticas en el ámbito de los juegos.
+                </p>
+                <p className="mt-8">
+                  Inspirados por la teoría de Poisson, abordamos las
+                  probabilidades de ganar para{" "}
+                  <span className="font-bold text-[#11f120]">N</span> jugadores
+                  con un enfoque serio y formal.
+                </p>
+              </div>
+              <div>
+                <p>
+                  Adéntrate con nosotros en este análisis meticuloso que destaca
+                  la importancia de la probabilidad{" "}
+                  <span className="font-bold text-[#11f120]">P</span> en la toma
+                  de decisiones informadas.
+                </p>
+              </div>
+            </div>
+            <dl className="mt-16 grid grid-cols-1 gap-x-8 gap-y-12 sm:mt-20 sm:grid-cols-2 sm:gap-y-16 lg:mt-28 lg:grid-cols-4">
+              {stats.map((stat, statIdx) => (
                 <div
-                    className="absolute inset-x-0 top-4 -z-10 flex transform-gpu justify-center overflow-hidden blur-3xl"
-                    aria-hidden="true"
+                  key={statIdx}
+                  className="flex flex-col-reverse gap-y-3 border-l border-white/20 pl-6"
                 >
-                    <div
-                        className="aspect-[1108/632] w-[69.25rem] flex-none bg-gradient-to-r from-[#80caff] to-[#11f120] opacity-25"
-                        style={{
-                            clipPath:
-                                'polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)',
-                        }}
-                    />
+                  <dt className="text-base leading-7 text-[#11f120]">
+                    {stat.label}
+                  </dt>
+                  <dd className="text-3xl font-semibold tracking-tight text-white">
+                    {stat.value}
+                  </dd>
                 </div>
+              ))}
+            </dl>
+          </div>
+        </div>
 
-                {/* Header section */}
-                <div className="px-6 pt-14 lg:px-8">
-                    <div className="mx-auto max-w-2xl pt-24 text-center sm:pt-40">
-                        <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">Probabilidad y estad&iacute;stica</h2>
-                        <p className="mt-6 text-lg leading-8 text-gray-300">
-                            Curso de probabilidad y estadistica de la Universidad Uruguay ORT 2023
-                        </p>
-                    </div>
-                </div>
+        {/* Image section */}
+        <div className="mt-32 sm:mt-40 xl:mx-auto xl:max-w-7xl xl:px-8">
+          <img
+            src="https://images.unsplash.com/photo-1521737852567-6949f3f9f2b5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2894&q=80"
+            alt=""
+            className="aspect-[9/4] w-full object-cover xl:rounded-3xl"
+          />
+        </div>
 
-                {/* Content section */}
-                <div className="mx-auto mt-20 max-w-7xl px-6 lg:px-8">
-                    <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
-                        <div className="grid max-w-xl grid-cols-1 gap-8 text-base leading-7 text-gray-300 lg:max-w-none lg:grid-cols-2">
-                            <div>
-                                <p>
-                                    Nos complace darle la bienvenida a nuestro proyecto centrado en las precisiones estadísticas en el ámbito de los juegos.
-                                </p>
-                                <p className="mt-8">
-                                    Inspirados por la teoría de Poisson, abordamos las probabilidades de ganar para <span className='font-bold text-[#11f120]'>N</span> jugadores con un enfoque serio y formal.
+        {/* Team section */}
 
-                                </p>
-                            </div>
-                            <div>
-                                <p>
-                                    Adéntrate con nosotros en este análisis meticuloso que destaca la importancia de la probabilidad <span className='font-bold text-[#11f120]'>P</span> en la toma de decisiones informadas.
-                                </p>
-                            
-                            </div>
-                        </div>
-                        <dl className="mt-16 grid grid-cols-1 gap-x-8 gap-y-12 sm:mt-20 sm:grid-cols-2 sm:gap-y-16 lg:mt-28 lg:grid-cols-4">
-                            {stats.map((stat, statIdx) => (
-                                <div key={statIdx} className="flex flex-col-reverse gap-y-3 border-l border-white/20 pl-6">
-                                    <dt className="text-base leading-7 text-[#11f120]">{stat.label}</dt>
-                                    <dd className="text-3xl font-semibold tracking-tight text-white">{stat.value}</dd>
-                                </div>
-                            ))}
-                        </dl>
-                    </div>
-                </div>
+        <Team />
 
-                {/* Image section */}
-                <div className="mt-32 sm:mt-40 xl:mx-auto xl:max-w-7xl xl:px-8">
-                    <img
-                        src="https://images.unsplash.com/photo-1521737852567-6949f3f9f2b5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2894&q=80"
-                        alt=""
-                        className="aspect-[9/4] w-full object-cover xl:rounded-3xl"
-                    />
-                </div>
-
-                {/* Team section */}
-
-                <Team />
-
-                {/* CTA section */}
-                {/* <div className="relative isolate -z-10 mt-32 sm:mt-40">
+        {/* CTA section */}
+        {/* <div className="relative isolate -z-10 mt-32 sm:mt-40">
                     <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
                         <div className="mx-auto flex max-w-2xl flex-col gap-16 bg-white/5 px-6 py-16 ring-1 ring-white/10 sm:rounded-3xl sm:p-8 lg:mx-0 lg:max-w-none lg:flex-row lg:items-center lg:py-20 xl:gap-x-20 xl:px-20">
                             <img
@@ -143,11 +156,11 @@ const Home = () => {
                         />
                     </div>
                 </div> */}
-            </main>
+      </main>
 
-            {/* Footer */}
-            <Footer />
-        </div>
-    )
-}
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
+};
 export default Home;
